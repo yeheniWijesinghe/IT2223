464cb102f4555406ee5d108485501e83ba3abd15
Yeheni Narthana Wijesinghe <yeheninw00@gmail.com>
2025-05-21 23:13:42 +0530

    Add files via upload

---
+++ Day_06/Codes/binarySearch.m
@@ -0,0 +1,25 @@
+A=[3,6,8,12,14,17,25,29,31,36,42,47,53,55,62];
+low=1;
+high=length(A);
+target=45;
+found = false;
+tic;
+while (low <= high)
+    mid = round((low + high) / 2);  % Ensure mid is an integer also floor() can use
+
+    if A(mid) == target
+        fprintf("Target found at index %d\n", mid);
+        found = true;
+        break;
+    elseif target > A(mid)
+        low = mid + 1;
+    else
+        high = mid - 1;
+    end
+end
+
+if ~found
+    fprintf("Target not found in the array!\n");
+end
+endtime=toc;
+fprintf("Elapsed time binary search: %fseconds\n",endtime);
---
+++ Day_06/Codes/linearSearch.m
@@ -0,0 +1,23 @@
+arr=[3,5,9,11,13];
+target=1;
+l=length(arr);
+tic;
+for i=1:l
+    if arr(i)==target
+        found=true;
+        index=i;
+        break;
+    else
+        found=false;
+   
+    end
+end
+
+if(found)
+    disp("Target found in index: "+index);
+else
+    disp("Target not found in the array!");
+end
+
+endtime=toc;
+fprintf("Elapsed time: %fseconds\n",endtime);
